ee6277fbd081ad9649ae48a8ea26f0b20d66d52a
wyrover <wyrover@gmail.com>
2017-03-16 23:17:32 +0800

    * main.cpp

--- src/test-datetime/main.cpp
+++ src/test-datetime/main.cpp
@@ -8,6 +8,8 @@
 #include <time.h>
 #include <errno.h>
 #include <vector>
+#include <stdlib.h>
+#include <string.h>
 
 #include <api/datetime.h>
 
@@ -391,13 +393,107 @@ rb_w32_uunlink(const char *path)
     return ret;
 }
 
+#if defined _WIN32 && !defined strncasecmp
+#define strncasecmp strnicmp
+#endif
 
+#define digit(x) ((x) >= '0' && (x) <= '9')
+#define strstart(s, n) (strncasecmp((s), (n), strlen(n)) == 0)
+
+#define C_CODESET "US-ASCII"     /* Return this as the encoding of the
+				  * C/POSIX locale. Could as well one day
+				  * become "UTF-8". */
+
+#if defined _WIN32 || defined __CYGWIN__
+#define JA_CODESET "Windows-31J"
+#else
+#define JA_CODESET "EUC-JP"
+#endif
+
+static char buf[16];
+
+const char *
+nl_langinfo_codeset(void)
+{
+  const char *l, *p;
+  int n;
+
+  if (((l = getenv("LC_ALL"))   && *l) ||
+      ((l = getenv("LC_CTYPE")) && *l) ||
+      ((l = getenv("LANG"))     && *l)) {
+    /* check standardized locales */
+    if (!strcmp(l, "C") || !strcmp(l, "POSIX"))
+      return C_CODESET;
+    /* check for encoding name fragment */
+    p = strchr(l, '.');
+    if (!p++) p = l;
+    if (strstart(p, "UTF"))
+	return "UTF-8";
+    if ((n = 5, strstart(p, "8859-")) || (n = 9, strstart(p, "ISO-8859-"))) {
+      if (digit(p[n])) {
+	p += n;
+	memcpy(buf, "ISO-8859-\0\0", 12);
+	buf[9] = *p++;
+	if (digit(*p)) buf[10] = *p++;
+	return buf;
+      }
+    }
+    if (strstart(p, "KOI8-R")) return "KOI8-R";
+    if (strstart(p, "KOI8-U")) return "KOI8-U";
+    if (strstart(p, "620")) return "TIS-620";
+    if (strstart(p, "2312")) return "GB2312";
+    if (strstart(p, "HKSCS")) return "Big5HKSCS";   /* no MIME charset */
+    if (strstart(p, "BIG5")) return "Big5";
+    if (strstart(p, "GBK")) return "GBK";           /* no MIME charset */
+    if (strstart(p, "18030")) return "GB18030";     /* no MIME charset */
+    if (strstart(p, "Shift_JIS") || strstart(p, "SJIS")) return "Windows-31J";
+    /* check for conclusive modifier */
+    if (strstart(p, "euro")) return "ISO-8859-15";
+    /* check for language (and perhaps country) codes */
+    if (strstart(l, "zh_TW")) return "Big5";
+    if (strstart(l, "zh_HK")) return "Big5HKSCS";   /* no MIME charset */
+    if (strstart(l, "zh")) return "GB2312";
+    if (strstart(l, "ja")) return JA_CODESET;
+    if (strstart(l, "ko")) return "EUC-KR";
+    if (strstart(l, "ru")) return "KOI8-R";
+    if (strstart(l, "uk")) return "KOI8-U";
+    if (strstart(l, "pl") || strstart(l, "hr") ||
+	strstart(l, "hu") || strstart(l, "cs") ||
+	strstart(l, "sk") || strstart(l, "sl")) return "ISO-8859-2";
+    if (strstart(l, "eo") || strstart(l, "mt")) return "ISO-8859-3";
+    if (strstart(l, "el")) return "ISO-8859-7";
+    if (strstart(l, "he")) return "ISO-8859-8";
+    if (strstart(l, "tr")) return "ISO-8859-9";
+    if (strstart(l, "th")) return "TIS-620";      /* or ISO-8859-11 */
+    if (strstart(l, "lt")) return "ISO-8859-13";
+    if (strstart(l, "cy")) return "ISO-8859-14";
+    if (strstart(l, "ro")) return "ISO-8859-2";   /* or ISO-8859-16 */
+    if (strstart(l, "am") || strstart(l, "vi")) return "UTF-8";
+    /* Send me further rules if you like, but don't forget that we are
+     * *only* interested in locale naming conventions on platforms
+     * that do not already provide an nl_langinfo(CODESET) implementation. */
+  }
+  return NULL;
+}
+
+//#define SIZEOF_CP_NAME ((sizeof(UINT) * 8 / 3) + 4)
+//#define CP_FORMAT(buf, codepage) snprintf(buf, sizeof(buf), "CP%u", (codepage))
 
 int main()
 {
 #ifdef DEBUG
     _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG | _CRTDBG_LEAK_CHECK_DF);
 #endif
+
+    /*const char *codeset = 0;
+    char cp[SIZEOF_CP_NAME];
+    UINT codepage = GetConsoleCP();
+    if (!codepage) codepage = GetACP();
+    CP_FORMAT(cp, codepage);
+    
+    std::cout << cp << std::endl;*/
+
+    getchar();
     
     return 0;
 }
